b29b884d1983753881088285e57e40c232c73628
Dmytro Hrabovskyi <dima.grabovskiy2000@gmail.com>
2025-04-28 23:57:48 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,25 @@
-# write your code here
+from app.main import check_password
+
+
+def test_should_accept_valid_password() -> None:
+    assert check_password("Pass@word1") is True
+
+
+def test_should_check_upper_letter() -> None:
+    assert check_password("qwertyu1@") is False
+
+
+def test_should_check_max_length() -> None:
+    assert check_password("Qwertyuiopasdfgh1@") is False
+
+
+def test_should_check_min_length() -> None:
+    assert check_password("Str0ng@") is False
+
+
+def test_should_check_special_symbols() -> None:
+    assert check_password("Password1") is False
+
+
+def test_should_check_non_latin_alphabet() -> None:
+    assert check_password("Пароль_1@") is False
